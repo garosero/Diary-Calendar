b97b86d6792de872a6441f09598918ecde4c0cc9
garosero <gangya250@gmail.com>
2021-01-05 22:29:37 +0900

    config

--- backend/passport/google.js
+++ backend/passport/google.js
@@ -1,5 +1,5 @@
 const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;  //oauth20 사용하면 internal error
+const GoogleStrategy = require("passport-google-oauth20").Strategy;  //oauth20 
 const bcrypt = require("bcrypt");
 const User = require("../models/user");
 const config = require("../config.json");
