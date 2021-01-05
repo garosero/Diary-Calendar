4995d9b657307398c2007b38837b5ba62978fd86
garosero <gangya250@gmail.com>
2021-01-05 20:25:50 +0900

    config modify

--- backend/passport/google.js
+++ backend/passport/google.js
@@ -2,15 +2,15 @@ const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;  //oauth20 사용하면 internal error
 const bcrypt = require("bcrypt");
 const User = require("../models/user");
-
+const config = require('../config.json');
 
 
 module.exports = () => {
     passport.use(
       new GoogleStrategy(
         {
-          clientID: process.env.GOOGLE_CLIENT_ID,
-          clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+          clientID: config.GOOGLE_CLIENT_ID,
+          clientSecret: config.GOOGLE_CLIENT_SECRET,
           callbackURL: "http://localhost:4000/api/user/google/callback",
           // scope: [
           //   "profile",
