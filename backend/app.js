4f2245499136a90ddfade26a1c9705523396471a
garosero <gangya250@gmail.com>
2021-01-06 00:24:27 +0900

    middleware http

--- backend/app.js
+++ backend/app.js
@@ -75,7 +75,13 @@ dbConnect();
 
 
 /* routing */
-
+ app.use(function (req, res, next) {
+   if (req.headers["x-forwarded-proto"] === "https") {
+     res.redirect("http://" + req.hostname + req.url);
+   } else {
+     next();
+   }
+ });
 
 
 const expressRouting = require('./routes/index');
