7f17202c6b7bc33fcaef1734d64d1cf7544afd3e
garosero <gangya250@gmail.com>
2021-01-06 14:08:50 +0900

    .env

--- backend/app.js
+++ backend/app.js
@@ -34,7 +34,7 @@ app.use(session({
     saveUninitialized : false,
     name : 'SID',
     unset:'destroy',
-    secret : config["COOKIE_SECRET"],
+    secret : process.env.COOKIE_SECRET,
     cookie : {
         httpOnly : true,
         secure : false,
