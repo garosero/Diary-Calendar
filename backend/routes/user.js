0651c7eb007652032a571ceb329654a0fecf8f56
garosero <gangya250@gmail.com>
2021-01-07 22:51:38 +0900

    /

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -108,7 +108,7 @@ router.post('/logout',isLoggedIn,(req,res)=>{
    (req, res,next) => {
      // res.send(JSON.stringify(req.user));
      console.log("success");
-     return res.redirect(process.env.absoluteURI+'/');
+     return res.redirect('/');
    }
  );
 
