d6ac2729eb068014b206148575aaab90f5d9984c
garosero <gangya250@gmail.com>
2021-01-14 00:19:19 +0900

    use redux tools developmentOnly

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@ import {BrowserRouter, useHistory} from 'react-router-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import createSagaMiddleware from 'redux-saga';
 import rootReducer from './reducers';
 import rootSaga from './sagas/index';
@@ -13,21 +13,16 @@ import { loadUserAPI } from './lib/api/auth';
 
 const sagaMiddleware = createSagaMiddleware(); //사가 미들웨어 만들기 
 const middlewares = [sagaMiddleware];
+
+
 //compose - middleware 여러개 합성
 // const enhancer = compose(
 //   applyMiddleware(...middlewares),
 //   window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined' ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) =>f,
 // );
 
-/** 배포할 땐 사가에 로그가 보여지는 것 보안에 위협. 
- * const enhancer = process.env.NODE_ENV === 'production' 
- * ? compose(
- *  applyMiddlewares(...middlewares),)
- * :
- *  compose (
- *    applyMiddleware(...middlewares),
- *    !options.isServer && window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined' ? window.__REDUX_DEVTOOLS_EXTENSION__() : 
- */
+
+ 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middlewares)));
 
 function loadUser(){
